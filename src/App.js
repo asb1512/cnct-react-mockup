fa54264d50f5f440e6b1ea629c5e353e9fed7b30
asb1512 <andrew@olympicmedia.com>
2022-03-26 01:43:21 -0400

    added bg animation; rendering Welcome

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { useEffect } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Header from './components/Header';
+import Welcome from './components/Welcome';
 
-const rotateBgGradient = () => {
-  for (let i = 0; i <= 360; i++) {
-    setTimeout(300);
-  }
-}
+const rotateBackground = keyframes`
+  0%{background-position:10% 0%}
+  50%{background-position:91% 100%}
+  100%{background-position:10% 0%}
+`;
 
 const AppWrapper = styled.div`
   width: 100%;
   min-height: 100vh;
-  background: linear-gradient(313deg, rgba(211,31,47,1) 0%, rgba(19,51,103,1) 90%);
+  background: linear-gradient(313deg, rgba(211,31,47,1) 0%, rgba(19,51,103,1) 71%);
+  animation: ${rotateBackground} 10s linear infinite;
+  background-size: 200% 200%;
 `;
 
 function App() {
   return (
     <AppWrapper>
       <Header />
+      <Welcome />
     </AppWrapper>
   );
 }
